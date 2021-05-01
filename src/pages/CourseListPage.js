6d29d6fec62443b1755330079e957b3775850ab9
andie5 <andieasa@googlemail.com>
2021-05-01 21:09:53 +0100

    wip: add server to courses app

---
+++ src/pages/CourseListPage.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addCourse } from "../actions";
+import "./CourseListPage.css";
+
+const CourseListPage = ({ courses, dispatch }) => {
+  // create new course
+  // list of courses
+  const [courseName, setCourseName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addCourse(courseName));
+  };
+  return courses.length === 0 ? (
+    <div>
+      <h1>Create Your First Course</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Pick a name</label>
+        <input
+          value={courseName}
+          onChange={(e) => setCourseName(e.target.value)}
+        />
+        <button type='submit'>Create Course</button>
+      </form>
+    </div>
+  ) : (
+    <div>
+      <ul>
+        {courses.map((course) => (
+          <li key={course.id}>{course.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { courses: state.courses };
+};
+
+export default connect(mapStateToProps)(CourseListPage);
---
+++ src/reducer.js
@@ -0,0 +1,22 @@
+import {
+  ADD_COURSE_BEGIN,
+  ADD_COURSE_SUCCESS,
+  ADD_COURSE_ERROR,
+} from "./actions";
+import produce from "immer";
+
+const initialState = {
+  courses: [],
+};
+
+const reducer = produce((draft, action) => {
+  switch (action.type) {
+    case ADD_COURSE_SUCCESS:
+      draft.courses.push(action.payload);
+      return;
+    default:
+      return;
+  }
+}, initialState);
+
+export default reducer;
